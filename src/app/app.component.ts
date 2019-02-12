85925f78e50415eaf34cb7e773665830fdc5052c
Johann Alberto Jimenez <johann.jimenez@satrack.com>
2019-02-12 14:06:53 -0500

    navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
 
-  constructor(translate: TranslateService) {
+  constructor(translate: TranslateService, public router: Router, matIconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     translate.setDefaultLang('es');
     translate.use('es');
-  }
 
+    matIconRegistry.addSvgIcon('arrow-back', sanitizer.bypassSecurityTrustResourceUrl('../assets/icons/arrow_back.svg'));
+    matIconRegistry.addSvgIcon('add', sanitizer.bypassSecurityTrustResourceUrl('../assets/icons/add.svg'));
+  }
 }
